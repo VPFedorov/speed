24bfb567d30fff29484d54a4db4a02855b9a3b6c
Ilya <ilya_dmitriev_1993@bk.ru>
2021-12-21 23:10:17 +0300

    add listing AnnualReport.h

--- AnnualReport.h
+++ AnnualReport.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "ExpenseRecord.h"
+#include "OrderList.h"
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <numeric> //дл¤ accumulate()
+#include <cctype>
+using namespace std;
+//////////////////класс AnnualReport///////////////////////
+// ласс годового отчета
+class AnnualReport
+{
+private:
+	//RentRecord* ptrRR; // записи доходов
+	ExpenseRecord* ptrER; // записи расходов
+	float expenses, income; // суммы доходов и расходов
+	OrderList* summa;
+	//float saa;
+public:
+	AnnualReport(ExpenseRecord*, OrderList*);
+	void display(); // отображение годового отчета
+};
+
