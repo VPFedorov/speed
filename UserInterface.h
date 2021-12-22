3f33c9463f07466cc01e31315d3335003fb1826f
Artur K <artur.kriveckiy@yandex.ru>
2021-12-22 22:32:35 +0300

    Add code in userinterface.h

--- UserInterface.h
+++ UserInterface.h
@@ -1,1 +1,33 @@
+#include "Global.h"
+#include "OrderList.h"
+#include "OrderInputScreen.h"
+#include "ExpenseRecord.h"
+#include "ExpenseInputScreen.h"
+#include "AnnualReport.h"
+#include <vector>
+#include <list>
+#include <string>
 #pragma once
+#include <numeric> //дл¤ accumulate()
+#include <cctype>
+using namespace std;
+
+////////////////// ласс UserInterface//////////////////////
+//√лавный класс дл¤ запуска приложени¤:
+//этот класс определ¤ет взаимодействие юзера с программой.
+class UserInterface
+{
+private:
+	OrderList* ptrOrderList;
+	OrderInputScreen* ptrOrderInputScreen;
+	ExpenseRecord* ptrExpenseRecord;
+	ExpenseInputScreen* ptrExpenseInputScreen;
+	AnnualReport* ptrAnnualReport;
+	char ch;
+	int cs;
+public:
+
+	UserInterface();
+	~UserInterface();
+	void interact();
+}; // конец класса userInterfac
